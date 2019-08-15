c14cdabcbc9c1a07dfab78c9bb013aee0dc46076
Bobby Schade <bschade.18@gmail.com>
2019-08-15 09:59:25 -0500

    Updates

---
+++ precache-manifest.574a5f021a944c79b61f998097748620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b2dafe35bc281ed2f9bf6beeb18e1c",
+    "url": "/todo-v1/index.html"
+  },
+  {
+    "revision": "a0409847eb0ffdabb293",
+    "url": "/todo-v1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "3cb18cc7a7b0e90c035b",
+    "url": "/todo-v1/static/js/2.c9364c7c.chunk.js"
+  },
+  {
+    "revision": "a0409847eb0ffdabb293",
+    "url": "/todo-v1/static/js/main.f9da151b.chunk.js"
+  },
+  {
+    "revision": "c572bb04a0e7180942d1",
+    "url": "/todo-v1/static/js/runtime~main.8818f970.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-v1/static/media/logo.5d5d9eef.svg"
+  }
+]);
