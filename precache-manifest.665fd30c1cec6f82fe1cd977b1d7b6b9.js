bc219b6827f9294a4eddd79a68f403afceeef878
Bobby Schade <bschade.18@gmail.com>
2019-09-24 10:49:37 -0500

    Updates

--- precache-manifest.665fd30c1cec6f82fe1cd977b1d7b6b9.js
+++ precache-manifest.665fd30c1cec6f82fe1cd977b1d7b6b9.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "238019c54f4eae748cc18aa57290f355",
+    "revision": "f03e8e8f27fc9486f01afab886b9a453",
     "url": "/todo-v1/index.html"
   },
   {
-    "revision": "3279d92df67c7a7a2845",
-    "url": "/todo-v1/static/css/main.0895dc0e.chunk.css"
+    "revision": "a2bfcefb596282d38e15",
+    "url": "/todo-v1/static/css/main.f88144c3.chunk.css"
   },
   {
     "revision": "7b822e7c9a56ef6b5561",
     "url": "/todo-v1/static/js/2.2a110f14.chunk.js"
   },
   {
-    "revision": "3279d92df67c7a7a2845",
+    "revision": "a2bfcefb596282d38e15",
     "url": "/todo-v1/static/js/main.5403a6af.chunk.js"
   },
   {
